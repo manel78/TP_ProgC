0361a96ca9ea94fddb6843c95323d690e1dddaa6
manel78 <144171701+manel78@users.noreply.github.com>
2025-12-01 14:25:07 +0100

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+
+    
+    printf("Addition : %d + %d = %d\n", a, b, a + b);
+    printf("Soustraction : %d - %d = %d\n", a, b, a - b);
+    printf("Multiplication : %d * %d = %d\n", a, b, a * b);
+    printf("Division : %d / %d = %d\n", a, b, a / b);
+    printf("Modulo : %d %% %d = %d\n\n", a, b, a % b);
+
+
+    printf("a == b : %d\n", (a == b));  // renvoie 0 (faux)
+    printf("a > b : %d\n", (a > b));    // renvoie 1 (vrai)
+
+    return 0;
+}
+opérateur.c
